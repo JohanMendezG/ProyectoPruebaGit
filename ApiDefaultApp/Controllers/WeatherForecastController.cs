a2d340dd988aa4c4967f3f1348260a6bb3d3e58e
Johan Mendez <jamg_90_@hotmail.com>
2021-05-30 15:48:20 -0500

    comentario de prueba

--- ApiDefaultApp/Controllers/WeatherForecastController.cs
+++ ApiDefaultApp/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace ApiDefaultApp.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        //este comentario es una prueba del git
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
